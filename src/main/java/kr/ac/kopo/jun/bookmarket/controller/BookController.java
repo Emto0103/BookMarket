3f66e5d2d7db7f34909275d14d9fe4ba1a0b1ebb
emto0103 <emto0103@gmail.com>
2025-04-30 11:42:47 +0900

    도서 등록 페이지 완성

--- src/main/java/kr/ac/kopo/jun/bookmarket/controller/BookController.java
+++ src/main/java/kr/ac/kopo/jun/bookmarket/controller/BookController.java
@@ -5,10 +5,13 @@ import kr.ac.kopo.jun.bookmarket.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 @Controller
 @RequestMapping(value = "/books")
@@ -44,5 +47,35 @@ public class BookController {
         List<Book> booksByCategory = bookService.getBookListByCategory(category);
         model.addAttribute("bookslist", booksByCategory);
         return "books";
+    }
+
+    @GetMapping("/filter/{bookFilter}")
+    public String requestBooksByFilter(@MatrixVariable(pathVar = "bookFilter") Map<String, List<String>> bookFilter, Model model){
+        Set<Book> booksByFilter = bookService.getBookListByFilter(bookFilter);
+        model.addAttribute("bookslist", booksByFilter);
+
+        return "books";
+    }
+
+    @GetMapping("/add")
+    public String requestAddBook(){
+        return "addBook";
+    }
+
+    @PostMapping("/add")
+    public String requestSubmitNewBook(@ModelAttribute("book") Book book){
+        bookService.setNewBook(book);
+        return "redirect:/books";
+    }
+
+    @ModelAttribute
+    public void addAttributes(Model model){
+        model.addAttribute("addTitle", "신규 도서 등록");
+    }
+
+    @InitBinder
+    public void initBinder(WebDataBinder binder){
+        binder.setAllowedFields("bookId", "name", "author", "unitprice", "publisher", "description" +
+                "category", "unitInStock", "releaseDate", "condition");
     }
 }
--- src/main/java/kr/ac/kopo/jun/bookmarket/service/BookService.java
+++ src/main/java/kr/ac/kopo/jun/bookmarket/service/BookService.java
@@ -3,9 +3,13 @@ package kr.ac.kopo.jun.bookmarket.service;
 import kr.ac.kopo.jun.bookmarket.domain.Book;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public interface BookService {
     List<Book> getAllBookList();
     Book getBookById(String bookId);
     List<Book> getBookListByCategory(String category);
+    Set<Book> getBookListByFilter(Map<String, List<String>> Filter);
+    void setNewBook(Book book);
 }
